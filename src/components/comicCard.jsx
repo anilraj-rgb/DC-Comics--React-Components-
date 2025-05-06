378b5b81ff522f4376de8ea9d38c06b66313427c
anilraj-rgb <anil.raj9727@gmail.com>
2025-05-06 16:11:10 +0200

    [UPD] add cards and create the new components

---
+++ src/components/comicCard.jsx
@@ -0,0 +1,11 @@
+const ComicCard = ({ thumb, series }) => {
+
+    return (
+      <div className='comic-card'>
+        <img src={thumb} alt="" />
+        <h5>{series}</h5>
+      </div>
+    )
+  }
+  
+  export default ComicCard
