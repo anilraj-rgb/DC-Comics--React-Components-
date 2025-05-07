b8db4209246f2018aae0923ec8102391caab9d17
anilraj-rgb <anil.raj9727@gmail.com>
2025-05-07 16:50:20 +0200

    [DONE] complite the bonus in the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,66 @@
+const links = [
+    {
+        id: 1,
+        href: "#",
+        label: "Characters",
+        current: false,
+    },
+    {
+        id: 2,
+        href: "#",
+        label: "Comics",
+        current: true,
+    },
+    {
+        id: 3,
+        href: "#",
+        label: "Movies",
+        current: false,
+    },
+    {
+        id: 4,
+        href: "#",
+        label: "TV",
+        current: false,
+    },
+    {
+        id: 4,
+        href: "#",
+        label: "Games",
+        current: false,
+    },
+    {
+        id: 4,
+        href: "#",
+        label: "Collectibles",
+        current: false,
+    },
+    {
+        id: 4,
+        href: "#",
+        label: "Videos",
+        current: false,
+    },
+    {
+        id: 4,
+        href: "#",
+        label: "Fans",
+        current: false,
+    },
+    {
+        id: 4,
+        href: "#",
+        label: "News",
+        current: false,
+    },
+    {
+        id: 4,
+        href: "#",
+        label: "Shop",
+        current: false,
+    }
+]
+
 const Header = () => {
     return <header className="container">
         <figure>
@@ -5,16 +68,11 @@ const Header = () => {
         </figure>
         <nav className="navbar">
             <ul className="nav-list">
-                <li><a href="#">CHARACTERS</a></li>
-                <li><a href="#">COMICS</a></li>
-                <li><a href="#">MOVIES</a></li>
-                <li><a href="#">TV</a></li>
-                <li><a href="#">GAMES</a></li>
-                <li><a href="#">COLLETIBLES</a></li>
-                <li><a href="#">VIDEOS</a></li>
-                <li><a href="#">FANS</a></li>
-                <li><a href="#">NEWS</a></li>
-                <li><a href="#">SHOP</a></li>
+                {links.map((link) => (
+                    <li key={link.id} className={`nav-item ${link.current ? 'active' : ''}`}>
+                        <a href={link.href}>{link.label}</a>
+                    </li>
+                ))}
             </ul>
         </nav>
     </header>
